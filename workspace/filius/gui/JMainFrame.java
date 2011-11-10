764ff20fe95775d4f2b3801933abd0ee7bcfb353
stefan <stefan@474e684e-6449-4a4e-957a-b1ce291be208>
2011-11-10 20:20:27 +0000

    change key handling when text field is focused, in particular ctrl+a is working now

--- workspace/filius/gui/JMainFrame.java
+++ workspace/filius/gui/JMainFrame.java
@@ -32,28 +32,24 @@
 package filius.gui;
 
 import java.awt.Dimension;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.MouseInfo;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.KeyboardFocusManager;
-import java.awt.KeyEventDispatcher;
-import java.awt.MouseInfo;
-
-import java.net.URL;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import filius.Main;
 import filius.gui.netzwerksicht.GUIKnotenItem;
-import filius.hardware.Kabel;
-import filius.hardware.knoten.*;
 import filius.rahmenprogramm.SzenarioVerwaltung;
 
 public class JMainFrame extends javax.swing.JFrame implements WindowListener, Observer {
@@ -63,148 +59,156 @@ public class JMainFrame extends javax.swing.JFrame implements WindowListener, Ob
 
     /** Creates new form NewJFrame */
     private JMainFrame() {
-    	this.addWindowListener(this);
-    	SzenarioVerwaltung.getInstance().addObserver(this);
-        initComponents();
-        
-        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher ( new KeyEventDispatcher()  {  
-          public boolean dispatchKeyEvent(KeyEvent e){  
-             if (e.getID() == KeyEvent.KEY_PRESSED) {
-	         if (!(e.getSource().getClass().getSimpleName().equals("JTextField"))) {
-//		   Main.debug.print("KEY dispatcher:\n"
-//							  + "\tkey:'"+e.getKeyCode()+"'\n"
-//							  + "\tmodifier: '"+e.getModifiers()+"'\n"
-//							  + "\tmodifierText: '"+KeyEvent.getKeyModifiersText(e.getModifiers())+"'\n"
-//							  + "\tkeyChar: '"+e.getKeyChar()+"'\n"
-//							  + "\tsourceType: '"+e.getSource().getClass().getSimpleName()+"'\n");
-		 }
-            	 /* ignore space bar pressing on buttons */
-            	 if( (e.getKeyChar() == KeyEvent.VK_SPACE) && 
-            		 (e.getSource().getClass().getSimpleName() == "JButton") ) { 
-            		 return true;   // no further action by 'clicking' involuntarily via the space bar; 
-            		 				// other shortcuts will be given for this 
-            	 }
-            	 /* delete item on deletion key press */
-            	 if( (e.getKeyChar() == KeyEvent.VK_DELETE) &&
-            		 !(e.getSource().getClass().getSimpleName().equals("JTextField")) &&
-            		 (frame.isFocused())) {
-            		 if(GUIContainer.getGUIContainer().getMarkierung().isVisible()) {   // several items are selected
-            			LinkedList itemlist = GUIContainer.getGUIContainer().getGUIKnotenItemList();
-        				JMarkerPanel auswahl = GUIContainer.getGUIContainer().getAuswahl();
-        				JScrollPane scrollPane = GUIContainer.getGUIContainer().getScrollPane();
-        				GUIKnotenItem tempitem;
-    					int tx, ty, twidth, theight;
-    					int minx = 999999, miny = 999999, maxx = 0, maxy = 0;
-    					LinkedList<Object> markedlist = new LinkedList<Object>();
-    					ListIterator it = itemlist.listIterator();
-    					while (it.hasNext()) {
-    						tempitem = (GUIKnotenItem) it.next();
-    						tx = tempitem.getImageLabel().getX();
-    						twidth = tempitem.getImageLabel().getWidth();
-    						ty = tempitem.getImageLabel().getY();
-    						theight = tempitem.getImageLabel().getHeight();
-    						if (   tx 
-    								> auswahl.getX() - GUIContainer.getGUIContainer().getAbstandLinks()
-    								  + scrollPane.getHorizontalScrollBar().getValue()
-    							&& tx + twidth 
-    							    < auswahl.getX()
-    										+ auswahl.getWidth()
-    										- GUIContainer.getGUIContainer().getAbstandLinks()
-    										+ scrollPane.getHorizontalScrollBar()
-    												.getValue()
-    							&& ty 
-    								> auswahl.getY() - GUIContainer.getGUIContainer().getAbstandOben()
-    										+ scrollPane.getVerticalScrollBar().getValue()
-    							&& ty + theight 
-    								< auswahl.getY() + auswahl.getHeight()
-    										- GUIContainer.getGUIContainer().getAbstandOben()
-    										+ scrollPane.getVerticalScrollBar().getValue()) {
-    							markedlist.add(tempitem);
-    						}
-    					}
-//						Main.debug.println("selected elements for deletion (via key press):");
-    					for (int i=0; i < markedlist.size(); i++) {
-//    						Main.debug.println("\t"+((GUIKnotenItem) markedlist.get(i)).getKnoten().getName());
-    						GUIEvents.getGUIEvents().itemLoeschen(((GUIKnotenItem) markedlist.get(i)).getImageLabel(),((GUIKnotenItem) markedlist.get(i)));
-    					}
-    					auswahl.setVisible(false);
-    					GUIContainer.getGUIContainer().getMarkierung().setVisible(false);
-    					return true;
-            		 }
-            		 else if(GUIEvents.getGUIEvents().getActiveItem() != null) {  // single item active
-//            			 Main.debug.println("KeyDispatcher:  delete item '"+(GUIEvents.getGUIEvents().getActiveItem()!=null ? 
-//            						 GUIEvents.getGUIEvents().getActiveItem().getKnoten().getName() : "<null>"));
-            			 GUIEvents.getGUIEvents().itemLoeschen(GUIEvents.getGUIEvents().getActiveItem().getImageLabel(),GUIEvents.getGUIEvents().getActiveItem());
-            			 return true;
-            		 }
-            		 //else Main.debug.println("DEL pressed, but nothing selected");
-            	 }  // del key
-                 if(e.getModifiers() == 2) {   // CTRL key pressed
-//                	 Main.debug.println("KeyDispatcher:   CTRL-Key pressed, waiting for additional key!");
-                	 switch (e.getKeyCode()) {
-                	 	case 78:  // N  (new)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+N recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btNeu");
-                	 		return true;
-                	 	case 79:  // O  (open)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+O recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btOeffnen");
-                	 		return true;
-                	 	case 83:  // S  (save file)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+S recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btSpeichern");
-                	 		return true;
-                	 	case 68:  // D  (development mode)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+D recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btEntwurfsmodus");
-                	 		return true;
-                	 	case 82:  // R  (run-time/simulation mode)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+R recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btAktionsmodus");
-                	 		return true;
-                	 	case 37:  // left arrow    (slower simulation)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+left recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().changeSlider(-1);
-                	 		return true;
-                	 	case 39:  // right arrow   (faster simulation)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+right recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().changeSlider(1);
-                	 		return true;
-                	 	case 87:  // W  (wizard for new modules)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+W recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btWizard");
-                	 		return true;
-                	 	case 72:  // H  (help)
-//                	 		Main.debug.println("KeyDispatcher:    CTRL+H recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btHilfe");
-                	 		return true;
-                	 	case 65:  // A  (about dialog)
-  //              	 		Main.debug.println("KeyDispatcher:    CTRL+A recognised");
-                	 		GUIContainer.getGUIContainer().getMenu().doClick("btInfo");
-                	 		return true;
-                	 }
-                 }  // CTRL key pressed, i.e., menu command
-                 if(e.getModifiers() == 8) {   // ALT key pressed; only makes sense for cables!
-                	 if (e.getKeyCode() == 49) { // key '1'  (cable)
-//                	 		Main.debug.println("KeyDispatcher:    ALT+1 recognised");
-                	 		GUIContainer.getGUIContainer().getKabelvorschau().setBounds(
-                	 				(int) (MouseInfo.getPointerInfo().getLocation().getX()
-                	 				- getLocationOnScreen().getX())
-        							- (GUIContainer.getGUIContainer().getKabelvorschau().getWidth() / 2), 
-        							(int) (MouseInfo.getPointerInfo().getLocation().getY()
-                	 				- getLocationOnScreen().getY())
-        							- (GUIContainer.getGUIContainer().getKabelvorschau().getHeight() / 2) -32,   // subtract titlebar (approx. 32px)
-        							GUIContainer.getGUIContainer().getKabelvorschau().getWidth(), 
-        							GUIContainer.getGUIContainer().getKabelvorschau().getHeight());
-//                	 		GUIContainer.getGUIContainer()
-                	 		GUIContainer.getGUIContainer().getKabelvorschau().setVisible(true);
-                	 		return true;
-                	 }
-                 }  // ALT key pressed, i.e., sidebar item selected
-             }  // KEY_PRESSED
-             return false;}  
-        }); 
-        aktualisiere();
+		this.addWindowListener(this);
+		SzenarioVerwaltung.getInstance().addObserver(this);
+		initComponents();
+
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+			public boolean dispatchKeyEvent(KeyEvent e) {
+				if (e.getID() == KeyEvent.KEY_PRESSED && !(e.getSource() instanceof JTextField)) {
+					System.out.print("KEY dispatcher:\n" + "\tkey:'" + e.getKeyCode() + "'\n" + "\tmodifier: '"
+					        + e.getModifiers() + "'\n" + "\tmodifierText: '"
+					        + KeyEvent.getKeyModifiersText(e.getModifiers()) + "'\n" + "\tkeyChar: '" + e.getKeyChar()
+					        + "'\n" + "\tsourceType: '" + e.getSource().getClass().getSimpleName() + "'\n");
+
+					/* ignore space bar pressing on buttons */
+					if ((e.getKeyChar() == KeyEvent.VK_SPACE)
+					        && (e.getSource().getClass().getSimpleName() == "JButton")) {
+						return true; // no further action by 'clicking'
+									 // involuntarily via the space bar;
+						             // other shortcuts will be given for this
+					}
+					/* delete item on deletion key press */
+					if ((e.getKeyChar() == KeyEvent.VK_DELETE) && (frame.isFocused())) {
+						if (GUIContainer.getMarkierung().isVisible()) { // several
+																						  // items
+																						  // are
+																						  // selected
+							LinkedList<Object> itemlist = GUIContainer.getGUIContainer().getGUIKnotenItemList();
+							JMarkerPanel auswahl = GUIContainer.getAuswahl();
+							JScrollPane scrollPane = GUIContainer.getGUIContainer().getScrollPane();
+							GUIKnotenItem tempitem;
+							int tx, ty, twidth, theight;
+							LinkedList<Object> markedlist = new LinkedList<Object>();
+							ListIterator<Object> it = itemlist.listIterator();
+							while (it.hasNext()) {
+								tempitem = (GUIKnotenItem) it.next();
+								tx = tempitem.getImageLabel().getX();
+								twidth = tempitem.getImageLabel().getWidth();
+								ty = tempitem.getImageLabel().getY();
+								theight = tempitem.getImageLabel().getHeight();
+								if (tx > auswahl.getX() - GUIContainer.getGUIContainer().getAbstandLinks()
+								        + scrollPane.getHorizontalScrollBar().getValue()
+								        && tx + twidth < auswahl.getX() + auswahl.getWidth()
+								                - GUIContainer.getGUIContainer().getAbstandLinks()
+								                + scrollPane.getHorizontalScrollBar().getValue()
+								        && ty > auswahl.getY() - GUIContainer.getGUIContainer().getAbstandOben()
+								                + scrollPane.getVerticalScrollBar().getValue()
+								        && ty + theight < auswahl.getY() + auswahl.getHeight()
+								                - GUIContainer.getGUIContainer().getAbstandOben()
+								                + scrollPane.getVerticalScrollBar().getValue()) {
+									markedlist.add(tempitem);
+								}
+							}
+							// Main.debug.println("selected elements for deletion (via key press):");
+							for (int i = 0; i < markedlist.size(); i++) {
+								// Main.debug.println("\t"+((GUIKnotenItem)
+								// markedlist.get(i)).getKnoten().getName());
+								GUIEvents.getGUIEvents().itemLoeschen(
+								        ((GUIKnotenItem) markedlist.get(i)).getImageLabel(),
+								        ((GUIKnotenItem) markedlist.get(i)));
+							}
+							auswahl.setVisible(false);
+							GUIContainer.getMarkierung().setVisible(false);
+							return true;
+						} else if (GUIEvents.getGUIEvents().getActiveItem() != null) { // single
+																					   // item
+																					   // active
+						// Main.debug.println("KeyDispatcher:  delete item '"+(GUIEvents.getGUIEvents().getActiveItem()!=null
+						// ?
+						// GUIEvents.getGUIEvents().getActiveItem().getKnoten().getName()
+						// : "<null>"));
+							GUIEvents.getGUIEvents().itemLoeschen(
+							        GUIEvents.getGUIEvents().getActiveItem().getImageLabel(),
+							        GUIEvents.getGUIEvents().getActiveItem());
+							return true;
+						}
+						// else
+						// Main.debug.println("DEL pressed, but nothing selected");
+					} // del key
+					if (e.getModifiers() == 2) { // CTRL key pressed
+					// Main.debug.println("KeyDispatcher:   CTRL-Key pressed, waiting for additional key!");
+						switch (e.getKeyCode()) {
+						case 78: // N (new)
+							// Main.debug.println("KeyDispatcher:    CTRL+N recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btNeu");
+							return true;
+						case 79: // O (open)
+							// Main.debug.println("KeyDispatcher:    CTRL+O recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btOeffnen");
+							return true;
+						case 83: // S (save file)
+							// Main.debug.println("KeyDispatcher:    CTRL+S recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btSpeichern");
+							return true;
+						case 68: // D (development mode)
+							// Main.debug.println("KeyDispatcher:    CTRL+D recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btEntwurfsmodus");
+							return true;
+						case 82: // R (run-time/simulation mode)
+							// Main.debug.println("KeyDispatcher:    CTRL+R recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btAktionsmodus");
+							return true;
+						case 37: // left arrow (slower simulation)
+							// Main.debug.println("KeyDispatcher:    CTRL+left recognised");
+							GUIContainer.getGUIContainer().getMenu().changeSlider(-1);
+							return true;
+						case 39: // right arrow (faster simulation)
+							// Main.debug.println("KeyDispatcher:    CTRL+right recognised");
+							GUIContainer.getGUIContainer().getMenu().changeSlider(1);
+							return true;
+						case 87: // W (wizard for new modules)
+							// Main.debug.println("KeyDispatcher:    CTRL+W recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btWizard");
+							return true;
+						case 72: // H (help)
+							// Main.debug.println("KeyDispatcher:    CTRL+H recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btHilfe");
+							return true;
+						case 65: // A (about dialog)
+							// Main.debug.println("KeyDispatcher:    CTRL+A recognised");
+							GUIContainer.getGUIContainer().getMenu().doClick("btInfo");
+							return true;
+						}
+					} // CTRL key pressed, i.e., menu command
+					if (e.getModifiers() == 8) { // ALT key pressed; only makes
+												 // sense for cables!
+						if (e.getKeyCode() == 49) { // key '1' (cable)
+						// Main.debug.println("KeyDispatcher:    ALT+1 recognised");
+							GUIContainer
+							        .getGUIContainer()
+							        .getKabelvorschau()
+							        .setBounds(
+							                (int) (MouseInfo.getPointerInfo().getLocation().getX() - getLocationOnScreen()
+							                        .getX())
+							                        - (GUIContainer.getGUIContainer().getKabelvorschau().getWidth() / 2),
+							                (int) (MouseInfo.getPointerInfo().getLocation().getY() - getLocationOnScreen()
+							                        .getY())
+							                        - (GUIContainer.getGUIContainer().getKabelvorschau().getHeight() / 2)
+							                        - 32, // subtract titlebar
+														  // (approx. 32px)
+							                GUIContainer.getGUIContainer().getKabelvorschau().getWidth(),
+							                GUIContainer.getGUIContainer().getKabelvorschau().getHeight());
+							// GUIContainer.getGUIContainer()
+							GUIContainer.getGUIContainer().getKabelvorschau().setVisible(true);
+							return true;
+						}
+					} // ALT key pressed, i.e., sidebar item selected
+				} // KEY_PRESSED
+				return false;
+			}
+		});
+		aktualisiere();
     }
 
     public static JMainFrame getJMainFrame() {
